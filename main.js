00f5abaa12a72a03a3d502f7772a64e8e1f7188b
Gabriel22K <111747286+Gabriel22K@users.noreply.github.com>
2024-04-16 11:13:24 -0300

    lRFO

--- main.js
+++ main.js
@@ -37,16 +37,21 @@ function calculaTempo(tempoObjetivo){
     horas %= 24;
     
     if (tempoFinal > 0){
-        return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos";
+        return [dias,horas,minutos,segundos]
     } else {
-        return "Prazo Finalizado";
+        return [0,0,0,0];
     }
 
 }
 
 function atualizaCronometro(){
-    for (let i=0; i<contadores.length;i++){
-        contadores[i].textContent = calculaTempo(tempos[i]);   
+
+    document.getElementById("dias0").textContent = calculaTempo(tempos[1])[0];
+    document.getElementById("horas0").textContent = calculaTempo(tempos[1])[1];  
+    document.getElementById("min0").textContent = calculaTempo(tempos[1])[2];
+    document.getElementById("seg0").textContent = calculaTempo(tempos[1])[3];
+ for (let i=0; i<contadores.length;i++){
+        //contadores[i].textContent = calculaTempo(tempos[i]);   
     }
 }
 
@@ -55,7 +60,7 @@ function comecaCronometro(){
     setInterval(atualizaCronometro,1000);
 }
 
-//comecaCronometro();
+comecaCronometro();
 
 
 
